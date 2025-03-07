542b688bfded586831f0582b6f6e7615dba70195
Léo Turpin <turpin_l@etna-alternance.net>
2025-03-07 13:25:11 +0100

    feat: adding auth and role

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -1,3 +1,4 @@
+import { requireAllRoles, requireAuth } from '@zcorp/shared-fastify';
 import { uploadContract } from '@zcorp/wheelz-contracts';
 
 import { config } from '../config.js';
@@ -7,50 +8,61 @@ const minioClient = new MinioClient(config);
 await minioClient.init();
 
 export const uploadRouter = server.router(uploadContract.upload, {
-  uploadFile: async (input) => {
-    const file = await input.request.file();
-    if (!file) {
+  uploadFile: {
+    hooks: {
+      onRequest: [requireAuth(), requireAllRoles(['admin'])],
+    },
+    handler: async (input) => {
+      const file = await input.request.file();
+      if (!file) {
+        return {
+          status: 400,
+          body: {
+            message: 'No file uploaded',
+          },
+        };
+      }
+      const url = await minioClient.uploadFile(file);
+      if (!url) {
+        return {
+          status: 400,
+          body: {
+            message: 'File upload failed',
+          },
+        };
+      }
       return {
-        status: 400,
+        status: 200,
         body: {
-          message: 'No file uploaded',
+          url,
         },
       };
-    }
-    const url = await minioClient.uploadFile(file);
-    if (!url) {
-      return {
-        status: 400,
-        body: {
-          message: 'File upload failed',
-        },
-      };
-    }
-    return {
-      status: 200,
-      body: {
-        url,
-      },
-    };
+    },
   },
-  deleteFile: async (input) => {
-    const url = input.body.url;
-    const parsedUrl = new URL(url);
-    const fileName = parsedUrl.pathname.split('/').pop();
-    if (!fileName) {
+
+  deleteFile: {
+    hooks: {
+      onRequest: [requireAuth(), requireAllRoles(['admin'])],
+    },
+    handler: async (input) => {
+      const url = input.body.url;
+      const parsedUrl = new URL(url);
+      const fileName = parsedUrl.pathname.split('/').pop();
+      if (!fileName) {
+        return {
+          status: 400,
+          body: {
+            message: 'Invalid file name',
+          },
+        };
+      }
+      await minioClient.deleteFile(fileName);
       return {
-        status: 400,
+        status: 200,
         body: {
-          message: 'Invalid file name',
+          message: 'File deleted',
         },
       };
-    }
-    await minioClient.deleteFile(fileName);
-    return {
-      status: 200,
-      body: {
-        message: 'File deleted',
-      },
-    };
+    },
   },
 });
